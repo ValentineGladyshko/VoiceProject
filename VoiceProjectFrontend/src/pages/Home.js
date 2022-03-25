0495d51a7cb6d795ecb784a3e85c1ac8011c6bcf
Valentine Gladyshko <valentine9022@gmail.com>
2022-03-25 13:34:37 +0200

    Added heading panel for home page

--- VoiceProjectFrontend/src/pages/Home.js
+++ VoiceProjectFrontend/src/pages/Home.js
@@ -9,7 +9,7 @@ function MyCarousel(props) {
         <Carousel>
             {props.slides.map((row, index) =>
                 <Carousel.Item interval={5000} key={index}>
-                    <Slide src={row.src} alt={row.alt} label={row.label} text={row.text}/>
+                    <Slide image={row.image} label={row.label} text={row.text} />
                 </Carousel.Item>)
             }
         </Carousel>
@@ -21,8 +21,8 @@ function Slide(props) {
         <div>
             <img
                 className="d-block w-100"
-                src={props.src}
-                alt={props.alt}
+                src={props.image.src}
+                alt={props.image.alt}
             />
             <Carousel.Caption>
                 <h3>{props.label}</h3>
@@ -32,36 +32,128 @@ function Slide(props) {
     );
 }
 
+function HeadingBlock(props) {
+    return (
+        <Col lg={4} className="col">
+            <img class="rounded-circle heading-image" src={props.image.src} alt={props.image.alt}/>
+            <h2>{props.heading}</h2>
+            <p>{props.text}</p>
+            <Button variant="secondary">{props.button.text}</Button>{' '}
+        </Col>
+    );
+}
+
+function HeadingPanel(props) {
+    return (
+        <Row>
+            {props.headingBlocks.map((row, index) =>
+                    <HeadingBlock key={index} image={row.image} button={row.button} heading={row.heading} text={row.text} />)
+            }
+        </Row>
+    );
+}
+
+function InfoBlock(props) {
+    return (
+        <Row className="featurette">
+            <Col md={7, { order: 2 }}>
+                <h2 class="featurette-heading">First featurette heading. <span class="text-muted">It'll blow your mind.</span></h2>
+                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+            </Col>
+            <Col md={5, { order: 2 }}>
+                <img class="featurette-image img-fluid mx-auto" alt="500x500" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22500%22%20height%3D%22500%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20500%20500%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_17fbc96ed7f%20text%20%7B%20fill%3A%23AAAAAA%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A25pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_17fbc96ed7f%22%3E%3Crect%20width%3D%22500%22%20height%3D%22500%22%20fill%3D%22%23EEEEEE%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22185.1171875%22%20y%3D%22261.1%22%3E500x500%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" />
+            </Col>
+        </Row>
+    );
+}
+const headingBlocks = [
+    {
+        image: {
+            src: "./images/image4.jpg",
+            alt: "Generic placeholder image"
+        },
+        heading: "Heading",
+        text: "Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.",
+        button: {
+            text: "View details"
+        }
+    },
+    {
+        image: {
+            src: "./images/image5.jpg",
+            alt: "Generic placeholder image"
+        },
+        heading: "Heading",
+        text: "Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.",
+        button: {
+            text: "View details"
+        }
+    },
+    {
+        image: {
+            src: "./images/image6.jpg",
+            alt: "Generic placeholder image"
+        },
+        heading: "Heading",
+        text: "Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.",
+        button: {
+            text: "View details"
+        }
+    },
+]
+
 const slides = [
-    { src: "./images/image.jpg", alt: "First slide", label: "First slide label", text: "Nulla vitae elit libero, a pharetra augue mollis interdum." },
-    { src: "./images/image2.jpg", alt: "Second slide", label: "Second slide label", text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit." },
-    { src: "./images/image3.jpg", alt: "Third slide", label: "Third slide label", text: "Praesent commodo cursus magna, vel scelerisque nisl consectetur." }
+    {
+        image: {
+            src: "./images/image.jpg",
+            alt: "First slide"
+        },
+        label: "First slide label",
+        text: "Nulla vitae elit libero, a pharetra augue mollis interdum."
+    },
+    {
+        image: {
+            src: "./images/image2.jpg",
+            alt: "Second slide"
+        },
+        label: "Second slide label",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+        image: {
+            src: "./images/image3.jpg",
+            alt: "Third slide"
+        },
+        label: "Third slide label",
+        text: "Praesent commodo cursus magna, vel scelerisque nisl consectetur."
+    }
 ];
 
 const Home = () => (
     <div>
         <MyCarousel slides={slides} />
         <Container className="marketing">
-            <Row>
-                <Col lg={4} className="col">
-                    <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />
-                    <h2>Heading</h2>
-                    <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
-                    <Button variant="secondary">View details</Button>{' '}
-                </Col>
-                <Col lg={4} className="col">
-                    <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />
-                    <h2>Heading</h2>
-                    <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
-                    <Button variant="secondary">View details</Button>{' '}
-                </Col>
-                <Col lg={4} className="col">
-                    <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />
-                    <h2>Heading</h2>
-                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-                    <Button variant="secondary">View details</Button>{' '}
-                </Col>
-            </Row>
+            <HeadingPanel headingBlocks={headingBlocks} />
+            {/*<Row>*/}
+            {/*    <Col lg={4} className="col">*/}
+            {/*        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />*/}
+            {/*        <h2>Heading</h2>*/}
+            {/*        <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>*/}
+            {/*        <Button variant="secondary">View details</Button>{' '}*/}
+            {/*    </Col>*/}
+            {/*    <Col lg={4} className="col">*/}
+            {/*        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />*/}
+            {/*        <h2>Heading</h2>*/}
+            {/*        <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>*/}
+            {/*        <Button variant="secondary">View details</Button>{' '}*/}
+            {/*    </Col>*/}
+            {/*    <Col lg={4} className="col">*/}
+            {/*        <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" />*/}
+            {/*        <h2>Heading</h2>*/}
+            {/*        <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>*/}
+            {/*        <Button variant="secondary">View details</Button>{' '}*/}
+            {/*    </Col>*/}
+            {/*</Row>*/}
 
             <hr class="featurette-divider" />
 
