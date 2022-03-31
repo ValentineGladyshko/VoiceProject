4dde08e0038097bbedc705b58251094e7da72e39
Valentine Gladyshko <valentine9022@gmail.com>
2022-03-31 13:43:20 +0300

    App and Navigation bar refactored from class to function

--- VoiceProjectFrontend/src/App.js
+++ VoiceProjectFrontend/src/App.js
@@ -1,6 +1,6 @@
 import { React, Component, useState } from 'react';
 import { Container, Navbar, Nav, Row, Col } from 'react-bootstrap';
-import { NavLink, Navigate, Routes, Route } from 'react-router-dom';
+import { NavLink, Navigate, Routes, Route, Outlet } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Home from './pages/Home';
@@ -17,53 +17,36 @@ import navLinksRu from './json/ru/navLinks.json';
 import navLinksEn from './json/en/navLinks.json';
 import footerContent from './json/footerContent.json';
 
-class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            navLinks: navLinksUa,
-            footerContent: footerContent,
-            language: 'en'
-        };
-        this.handleLanguageChange = this.handleLanguageChange.bind(this);
-    }
-
-    handleLanguageChange(language) {
-        //const navigate = useNavigate();
-        this.setState({ language });
-        if (language === 'ru') {
-            this.setState({ navLinks: navLinksRu });
-            window.history.replaceState(null, "New Page Title", "/ru")
-        }
-        else if (language === 'en') {
-            this.setState({ navLinks: navLinksEn });
-            window.history.replaceState(null, "New Page Title", "/en")
-        }
-        else {
-            this.setState({ navLinks: navLinksUa });
-            window.history.replaceState(null, "New Page Title", "/ua")
-        }
-    }
-
-    render() {
-        return (<div className="App">
-            <NavigationBar navLinks={this.state.navLinks} onLanguageChange={this.handleLanguageChange} />
-            <Main />
-            <FooterBar footerContent={this.state.footerContent} />
-        </div>);
-    }
+function App() {
+    return (<div className="App">
+        <Routes>
+            <Route path='/' element={<Navigate to="/en" replace />}></Route>
+            <Route path='/ua' element={<Main navLinks={navLinksUa} footerContent={footerContent} />}>
+                <Route index element={<Home slides={slides} headingBlocks={headingBlocks} infoBlocks={infoBlocks} />} />
+                <Route path="album" element={<Album thumbnailsCollection={thumbnailsCollection} />} />
+            </Route>
+            <Route path='/ru' element={<Main navLinks={navLinksRu} footerContent={footerContent} />}>
+                <Route index element={<Home slides={slides1} headingBlocks={headingBlocks} infoBlocks={infoBlocks} />} />
+                <Route path="album" element={<Album thumbnailsCollection={thumbnailsCollection} />} />
+            </Route>
+            <Route path='/en' element={<Main navLinks={navLinksEn} footerContent={footerContent} />}>
+                <Route index element={<Home slides={slides} headingBlocks={headingBlocks} infoBlocks={infoBlocks} />} />
+                <Route path="album" element={<Album thumbnailsCollection={thumbnailsCollection} />} />
+            </Route>
+        </Routes>
+    </div>);
 }
 
-const Main = () => (
-    <Routes>
-        <Route path='/' element={<Navigate to="/en" replace />}></Route>
-        <Route path='/ua' element={<Home slides={slides} headingBlocks={headingBlocks} infoBlocks={infoBlocks} />}></Route>
-        <Route path='/ru' element={<Home slides={slides1} headingBlocks={headingBlocks} infoBlocks={infoBlocks} />}></Route>
-        <Route path='/en' element={<Home slides={slides} headingBlocks={headingBlocks} infoBlocks={infoBlocks} />}></Route>
-        <Route path='/ua/album' element={<Album thumbnailsCollection={thumbnailsCollection} />}></Route>
-        <Route path='/ru/album' element={<Album thumbnailsCollection={thumbnailsCollection} />}></Route>
-        <Route path='/en/album' element={<Album thumbnailsCollection={thumbnailsCollection} />}></Route>
-    </Routes>
-);
+
+
+function Main(props) {
+    return (
+        <>
+            <NavigationBar navLinks={props.navLinks} />
+            <Outlet />
+            <FooterBar footerContent={props.footerContent} />
+        </>
+    );
+}
 
 export default App;
--- VoiceProjectFrontend/src/pages/NavigationBar.js
+++ VoiceProjectFrontend/src/pages/NavigationBar.js
@@ -3,49 +3,34 @@ import { Container, Nav, Navbar, ButtonGroup, ToggleButton } from 'react-bootstr
 import { NavLink, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-class NavigationBar extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            navLinks: props.navLinks
-        }
+function NavigationBar(props) {
 
-        this.handleLanguageChange = this.handleLanguageChange.bind(this);
-    }
+    const languages = [
+        { name: 'Ua', href: '/ua' },
+        { name: 'Ru', href: '/ru' },
+        { name: 'En', href: '/en' },
+    ];
 
-    handleLanguageChange(language) {
-        this.props.onLanguageChange(language);
-    }
-
-    render() {
-        const languages = [
-            { name: 'Ua', value: 'ua', href: '/ua' },
-            { name: 'Ru', value: 'ru', href: '/ru' },
-            { name: 'En', value: 'en', href: '/en' },
-        ];
-
-        return (
-            <Navbar bg="dark" variant="dark" sticky="top">
-                <Container>
-                    <Nav className="me-auto">
-                        <NavLinks navLinks={this.props.navLinks} />
-                    </Nav>
-                    <Nav>
-                        {languages.map((language, index) => (
-                            <NavLink
-                                key={index}
-                                to={language.href}
-                                className="nav-link"
-                                onClick={() => this.handleLanguageChange(language.value)}
-                            >
-                                {language.name}
-                            </NavLink>
-                        ))}
-                    </Nav>
-                </Container>
-            </Navbar>
-        );
-    }
+    return (
+        <Navbar bg="dark" variant="dark" sticky="top">
+            <Container>
+                <Nav className="me-auto">
+                    <NavLinks navLinks={props.navLinks} />
+                </Nav>
+                <Nav>
+                    {languages.map((language) => (
+                        <NavLink
+                            key={language.name}
+                            to={language.href}
+                            className="nav-link"
+                        >
+                            {language.name}
+                        </NavLink>
+                    ))}
+                </Nav>
+            </Container>
+        </Navbar>
+    );
 }
 
 function NavLinks(props) {
